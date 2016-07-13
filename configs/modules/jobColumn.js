2f11b4879e949f0d62f96561a3529839218bd997
676702008@qq.com <676702008@qq.com>
2016-07-13 20:38:14 +0800

    modify column config

--- configs/modules/jobColumn.js
+++ configs/modules/jobColumn.js
@@ -1,14 +1,14 @@
 module.exports = [
     {field: 'Props.Name', title: '作业名称', width: 350},
-    {field: 'Props.User', title: '所有者', width: 150},
+    {field: 'Props.User', title: '所有者', width: 80},
     {field: 'Props.Frames', title: '帧序范围', width: 200},
     {
-        field: 'DateStart', title: '开始时间', width: 200, formatter: function (val) {
+        field: 'DateStart', title: '开始时间', width: 160, formatter: function (val) {
         return Calendar.getInstance(val).format('yyyyMMdd HH:mm:ss');
     }
     },
     {
-        field: 'DateComp', title: '结束时间', width: 200, formatter: function (val) {
+        field: 'DateComp', title: '结束时间', width: 160, formatter: function (val) {
         var value = Calendar.getInstance(val).format('yyyyMMdd HH:mm:ss');
         if(value == '20010101 00:00:00')
             return "----"
@@ -16,8 +16,9 @@ module.exports = [
             return value;
     }
     },
+    {field: 'Aux.0', title: '场景文件', width: 300},
     {
-        field: 'SnglTskPrg', title: '状态', width: 200, formatter: function (val,row) {
+        field: 'SnglTskPrg', title: '状态', width: 100, formatter: function (val,row) {
         var value = Calendar.getInstance(row.DateComp).format('yyyyMMdd HH:mm:ss');
         if(value == '20010101 00:00:00')
             return "正在运行"
@@ -26,6 +27,5 @@ module.exports = [
     }, formatter_back: function (val) {
         return val;
     }
-    },
-    {field: 'Aux.0', title: '场景文件', width: 350}
+    }
 ];
